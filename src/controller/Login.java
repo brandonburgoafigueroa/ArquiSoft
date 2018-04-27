3fc7acd163dc7f26938c27b0efde79fe01d93063
lopolafa <brandonburgoafigueroa@gmail.com>
2018-04-26 20:39:46 -0400

    Login refactor

--- src/controller/Login.java
+++ src/controller/Login.java
@@ -4,26 +4,56 @@ public class Login implements IState{
     private Mailbox currentMailbox;
     private String accumulatedKeys="";
     Connection connection;
+
     Login(Connection connection){
         this.connection=connection;
+        this.currentMailbox=connection.getCurrentMailbox();
     }
     @Override
     public void start(String command) {
-        this.currentMailbox=connection.getCurrentMailbox();
         if (itIsANumeralCharacter(command))
         {
-            if (currentMailbox.checkPasscode(accumulatedKeys))
-            {
-                connection.setStatus(new MailboxMenu(connection));
-                connection.updateObservables(MAILBOX_MENU_TEXT);
-            }
-            else {
-                connection.updateObservables(INCORRECT_PASSCODE_MESSAGE);
-            }
-            accumulatedKeys = "";
+            openMailboxMenu();
         }
-        else
-            accumulatedKeys += command;
+        else {
+            saveActualCommand(command);
+        }
+    }
+
+    private void openMailboxMenu() {
+        if (isTheCorrectPasscodeOfCurrentMailbox())
+        {
+            changeToMailboxMenuState();
+            showMailboxMenuText();
+        }
+        else {
+            showIncorrectPasscodeMessage();
+            cleanAccumulatedKeys();
+        }
+    }
+
+    private void saveActualCommand(String command) {
+        accumulatedKeys += command;
+    }
+
+    private void cleanAccumulatedKeys() {
+        accumulatedKeys = "";
+    }
+
+    private void showIncorrectPasscodeMessage() {
+        connection.updateObservables(INCORRECT_PASSCODE_MESSAGE);
+    }
+
+    private void showMailboxMenuText() {
+        connection.updateObservables(MAILBOX_MENU_TEXT);
+    }
+
+    private void changeToMailboxMenuState() {
+        connection.setStatus(new MailboxMenu(connection));
+    }
+
+    private boolean isTheCorrectPasscodeOfCurrentMailbox() {
+        return currentMailbox.checkPasscode(accumulatedKeys);
     }
 
     @Override
