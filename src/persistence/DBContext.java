a7f8deb9d119f57e60dd3de3eb5996ca5b5f1cee
christianfp <christian_flores995@hotmail.com>
2018-05-04 12:42:33 -0400

    Conexion con db implementado

--- src/persistence/DBContext.java
+++ src/persistence/DBContext.java
@@ -1,4 +1,31 @@
 package persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class DBContext {
+    String url = "";
+    Connection dbConnection;
+    public void connect(){
+        try {
+                dbConnection= DriverManager.getConnection("jdbc:sqlite:"+url);
+                if (dbConnection!=null){
+                    System.out.println("Conectado");
+                }
+            }
+        catch(SQLException ex) {
+            System.out.println("Fallo:" + ex);
+        }
+    }
+    public void close(){
+        try{
+            dbConnection.close();
+        }
+        catch(SQLException ex){
+            Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
 }
