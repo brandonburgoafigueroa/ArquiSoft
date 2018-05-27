0087679c97bb66522b29ac740e03d9e8b73b8752
christianfp <christian_flores995@hotmail.com>
2018-05-27 01:13:23 -0400

    currentMessage y executeCommand services impleentados

--- src/api/VoiceMailService.java
+++ src/api/VoiceMailService.java
@@ -9,9 +9,25 @@ public class VoiceMailService {
     }
     public VoiceMailService(){
     }
+    public void hello(){
+        String currentMessage = "hello";
+        get("/currentMessage", (req, res) -> currentMessage);
+    }
     public void currentMessage(){
         String currentMessage = connection.getCurrentMailbox().getCurrentMessage().getText();
         get("/currentMessage", (req, res) -> currentMessage);
     }
+    public void executeCommand(){
+        get("/currentMessage", (req, res) ->{
+            String id = req.params(":id");
+            return connection.executeCommand(id);
+        });
+    }
+    public void currentGreeting(){
+        String greeting = connection.getCurrentMailbox().getGreeting();
+        get("/currentMessage", (req, res) -> greeting);
+    }
+
+
 
 }
