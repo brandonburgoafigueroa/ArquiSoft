29b22efc9ad4c595768af70e22ed205ddf587005
christianfp <christian_flores995@hotmail.com>
2018-05-28 10:17:45 -0400

    Heroku Assined port creado

--- src/main/java/com/arqui/VoiceMailService.java
+++ src/main/java/com/arqui/VoiceMailService.java
@@ -1,6 +1,21 @@
 package com.arqui;
 
 public class VoiceMailService {
+    private Connection connection;
+    public VoiceMailService(Connection connection){
+        this.connection = connection;
+    }
+    public VoiceMailService(){
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
 
 
 }
