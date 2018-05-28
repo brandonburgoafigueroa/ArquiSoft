b9d4f89050ab5b85a058af7af785a52067a2ca80
lopolafa <brandonburgoafigueroa@gmail.com>
2018-05-28 13:01:40 -0400

    Options

--- src/main/java/com/arqui/DisplayState/DisplayConnect.java
+++ src/main/java/com/arqui/DisplayState/DisplayConnect.java
@@ -1,4 +1,13 @@
 package com.arqui.DisplayState;
 
-public class DisplayConnect {
+import java.util.ArrayList;
+
+public final class DisplayConnect {
+    private static ArrayList<Option> Options=new ArrayList<Option>(){
+        {
+            add(new Option("Enter mailbox number followed by #", true));
+
+        }
+    };
+    private static Option Error=new Option("Incorrect mailbox number. Try again!", true);
 }
--- src/main/java/com/arqui/DisplayState/Option.java
+++ src/main/java/com/arqui/DisplayState/Option.java
@@ -1,4 +1,19 @@
 package com.arqui.DisplayState;
 
-public class Options {
+public class Option {
+    private String Text;
+    private boolean IsReadOnly;
+
+    public Option(String Text, boolean IsReadOnly) {
+     this.Text=Text;
+     this.IsReadOnly=IsReadOnly;
+    }
+
+    public String getText() {
+        return Text;
+    }
+
+    public boolean isReadOnly() {
+        return IsReadOnly;
+    }
 }
