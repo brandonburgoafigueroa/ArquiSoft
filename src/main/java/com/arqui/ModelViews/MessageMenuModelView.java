1686c985a448bf1ecaaa8b159612ae804ff5e2e3
lopolafa <brandonburgoafigueroa@gmail.com>
2018-06-03 18:52:28 -0400

    todo funcionando al igual que la api

--- src/main/java/com/arqui/ModelViews/MessageMenuModelView.java
+++ src/main/java/com/arqui/ModelViews/MessageMenuModelView.java
@@ -10,12 +10,17 @@ public class MessageMenuModelView implements IModelView {
     public MessageMenuModelView ()
     {
         Options=new ArrayList<>();
+        setOptions();
+        Information ="";
+    }
+
+    private void setOptions() {
         Options.add("listen to the current message");
         Options.add("save the current message");
         Options.add("delete the current message");
         Options.add("return to the main menu");
-        Information ="";
     }
+
     @Override
     public ArrayList<String> getOptions() {
         return Options;
@@ -28,5 +33,6 @@ public class MessageMenuModelView implements IModelView {
     public MessageMenuModelView(String Information) {
         this.Information=Information;
         Options=new ArrayList<>();
+        setOptions();
     }
 }
