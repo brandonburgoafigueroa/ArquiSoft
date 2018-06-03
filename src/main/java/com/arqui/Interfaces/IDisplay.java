312011f09b61a10d66c2ce44d2a425192aebb3de
lopolafa <brandonburgoafigueroa@gmail.com>
2018-06-03 17:17:59 -0400

    limpiando

--- src/main/java/com/arqui/Interfaces/IDisplay.java
+++
@@ -1,9 +0,0 @@
-package com.arqui.Interfaces;
-
-import java.util.ArrayList;
-
-public interface IDisplay {
-    ArrayList<String> getOptions();
-    String getError(String errorName);
-    String getText(String textName);
-}
