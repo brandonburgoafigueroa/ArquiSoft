98074fe9711438c461792fffb887baf5498ffd14
christianfp <christian_flores995@hotmail.com>
2018-06-04 01:05:16 -0400

    Voice mail service comments

--- src/main/java/com/arqui/Api/VoiceMailService.java
+++ src/main/java/com/arqui/Api/VoiceMailService.java
@@ -22,7 +22,7 @@ public class VoiceMailService implements IApi {
         if (processBuilder.environment().get("PORT") != null) {
             return Integer.parseInt(processBuilder.environment().get("PORT"));
         }
-        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+        return 4567;
     }
     public void currentMessage(){
         get("/currentMessage", (req, res) ->
